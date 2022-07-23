15d20b6c17c9e2a7977024f6e0c848d6cdf69c37
Begelit <dmilevch@gmail.com>
2022-07-23 12:41:15 +0300

    trainer

--- trainer.py
+++ trainer.py
@@ -251,9 +251,9 @@ class Tester():
         test_avgf = AvgMeter()
         test_s_m = AvgMeter()
         t = time.time()
-
-	Eval_tool = Evaluation_metrics(self.args.dataset, self.device)
-
+        
+        Eval_tool = Evaluation_metrics(self.args.dataset, self.device)
+	
         with torch.no_grad():
             #for i, (np_images, images, original_size, image_name) in enumerate(tqdm(self.test_loader)):
             #for i, (images, masks, original_size, image_name) in enumerate(tqdm(self.test_loader)):
