024a4b8a2de2e4557c7bb94fad723288fac027a1
Begelit <dmilevch@gmail.com>
2022-07-28 15:59:55 +0300

    train

--- trainer.py
+++ trainer.py
@@ -291,7 +291,7 @@ class Tester():
                     	os.makedirs("./outputs_imgs/removed_background/",exist_ok=True)
                     	#output = output.squeeze()*255.0  # convert uint8 type
                     	output = (output.squeeze().detach().cpu().numpy()*255.0).astype(np.uint8)
-                    	img = cv2.cvtColor(cv2.imread(os.path.join(self.te_img_folder_pred,image_name[i])),cv2.COLOR_BGR2RGB)
+                    	img = cv2.cvtColor(cv2.imread(os.path.join(self.te_img_folder_pred,image_name[i]+'.png')),cv2.COLOR_BGR2RGB)
                     	removed_bg_imgs = np.where(output.reshape((h,w,1))>0,img,255)
                     	#img = img.squeeze().detach().cpu().numpy()
                     	#img = img.squeeze().permute(1,2,0).cpu().numpy()
